23e5ce54f1b677874aa2f106a8ba6a651c1ae8ae
Seoulless25 <k.j.jackson1016@gmail.com>
2023-09-08 12:14:31 -0400

    Added model and controller

---
+++ controllers/api/poems.js
@@ -0,0 +1,49 @@
+const User = require('../..models/user');
+const Poem = require('../../models/poem');
+
+module.exports = {
+    create,
+    index,
+    delete: deletePoem,
+    edit
+};
+
+async function create(req, res) {
+    try {
+        const newPoem = {
+            text: req.body.newPoem,
+            user: req.user._id
+        };
+        const poem = await Poem.create(newPoem);
+        res.json(poem);
+    }   catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+async function index(req, res) {
+    try {
+        const poems = await Poem.find({});
+        res.json(poems);
+    }   catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+async function deletePoem(req, res) {
+    try {
+        const poem = await Poem.findOneAndDelete({_id: req.params.id});
+        res.json(poem);
+    }   catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+async function edit(req, res) {
+    try {
+        const poem = await Poem.findOneAndUpdate({ _id: req.params.id }, { text: req.body.editPoem }, { new: true});
+        res.json(poem);
+    }   catch (err) {
+        res.status(400).json(err);
+    }
+}
---
+++ models/poem.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const poemSchema = new Schema( {
+    text: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Poem', poemSchema);
